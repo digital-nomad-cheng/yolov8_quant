fb0989130c9dbebb9530f4da240b8968c4bde322
digital-nomad-cheng <vincentcheng929@gmail.com>
2024-07-30 22:13:56 +0200

    Update launch docker script.

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,2 +1,3 @@
 # docker run -it -v ./:/workspace --gpus all yolov8_quant:latest
-docker run -it --gpus all -v ./:/workspace yolov8_quant:latest
+# docker run -it --gpus all -v ./:/workspace yolov8_quant:latest
+docker run -it --gpus all --shm-size=8gb --cap-add=SYS_ADMIN -v ./:/workspace yolov8_quant:dev
