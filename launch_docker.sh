a5cdfbd9a84929232920ac751064f5eba54817b7
digital-nomad-cheng <vincentcheng929@gmail.com>
2024-09-08 00:46:55 +0200

    Update docker.

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,3 +1,4 @@
 # docker run -it -v ./:/workspace --gpus all yolov8_quant:latest
 # docker run -it --gpus all -v ./:/workspace yolov8_quant:latest
-docker run -it --gpus all --shm-size=8gb --cap-add=SYS_ADMIN -v ./:/workspace yolov8_quant:dev
+# docker run -it --gpus all --shm-size=8gb --cap-add=SYS_ADMIN -v ./:/workspace dlmc:work
+docker run -it --gpus all --shm-size=8gb --cap-add=SYS_ADMIN -v ./:/workspace nvcr.io/nvidia/pytorch:23.05-py3
