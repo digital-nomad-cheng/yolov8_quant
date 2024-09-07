d431b84a86a5c4cf51b0ab93ccb5897089133c6b
digital-nomad-cheng <vincentcheng929@gmail.com>
2024-09-07 18:48:00 +0200

    Update.

--- export_onnx.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-
-# Export the model
-model.export(format='onnx', simplify=True)
