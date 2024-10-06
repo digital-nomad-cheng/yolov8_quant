cb37d5381ff0af32a3b5f4189f6c8775658710a9
digital-nomad-cheng <vincentcheng929@gmail.com>
2024-10-06 14:53:30 +0200

    Update.

--- ptq/yolov8/export_onnx.py
+++ ptq/yolov8/export_onnx.py
@@ -4,4 +4,8 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
 # Export the model
-model.export(format='onnx', simplify=True)
+model.export(format='onnx')
+
+# Export tflite model
+# This will also generate a onnx file but is different from above for tflite compatabilty
+model.export(format='tflite', int8=True)
