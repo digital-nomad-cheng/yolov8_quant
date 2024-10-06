93464cef8009c07631708e372132ccf13aaf1e06
digital-nomad-cheng <vincentcheng929@gmail.com>
2024-10-06 15:08:50 +0200

    Update backend.

---
+++ ptq/yolov8/backend/__init__.py
@@ -0,0 +1,3 @@
+from .onnx_backend import load_onnx_model, run_onnx_model
+from .tensorrt_backend import load_tensorrt_model, run_tensorrt_model
+from .tflite_backend import load_tflite_model, run_tflite_model
--- ptq/yolov8/infer.py
+++ ptq/yolov8/infer.py
@@ -3,9 +3,11 @@ from loguru import logger
 from abc import abstractmethod, ABCMeta
 import cv2
 
-from onnx_backend import load_onnx_model, run_onnx_model
-from tensorrt_backend import load_tensorrt_model, run_tensorrt_model
-from tflite_backend import load_tflite_model, run_tflite_model
+from backend import (
+    load_onnx_model, run_onnx_model,
+    load_tensorrt_model, run_tensorrt_model,
+    load_tflite_model, run_tflite_model
+)
 
 class Infer(metaclass=ABCMeta):
     """
